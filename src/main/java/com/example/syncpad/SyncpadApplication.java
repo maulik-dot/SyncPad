4bc3477364fd9fdb2fa0ce08fdbe7370712e2359
Maulik Mahey <maulikmahey@Mauliks-MacBook-Air.local>
2026-07-26 01:22:35 +0530

    Initial commit: Set up Spring Boot Maven project with PostgreSQL and custom port 8082

---
+++ src/main/java/com/example/syncpad/SyncpadApplication.java
@@ -0,0 +1,13 @@
+package com.example.syncpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncpadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncpadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/syncpad/SyncpadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.syncpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncpadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
